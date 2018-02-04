165aefa134a70462a510eac2e8348177eb73e55d
Adam Gieron <adam.gieron@fastwhitecat.com>
2018-02-04 13:59:32 +0100

    Add read more/less link

--- public/js/custom.js
+++ public/js/custom.js
@@ -26,44 +26,6 @@
 $('.mce-notification-inner').remove();
 $('.mce-notification-warning').css('display','none');
 
-
-$(function() {
-    $(".img-w").each(function() {
-        $(this).wrap("<div class='img-c'></div>");
-        let imgSrc = $(this).find("img").attr("src");
-        $(this).css('background-image', 'url(' + imgSrc + ')');
-    })
-
-
-    $(".img-c").click(function() {
-        let w = $(this).outerWidth();
-        let h = $(this).outerHeight();
-        let x = $(this).offset().left;
-        let y = $(this).offset().top;
-
-
-        $(".gal-active").not($(this)).remove();
-        let copy = $(this).clone();
-        copy.insertAfter($(this)).height(h).width(w).delay(500).addClass("gal-active");
-        $(".gal-active").css('top', y - 228);
-        $(".gal-active").css('left', x - 285);
-
-        setTimeout(function() {
-            copy.addClass("positioned")
-        }, 0)
-
-    })
-});
-
-$(document).on("click", ".img-c.gal-active", function() {
-    let copy = $(this);
-    copy.removeClass("positioned gal-active").addClass("postactive");
-    setTimeout(function() {
-        copy.remove();
-    }, 500)
-})
-
-
 tinymce.init({
     selector: "#post-content",
     height: 500,
@@ -141,4 +103,15 @@ tinymce.init({
             $("#div").show();
         }, 1000);
     }
+});
+
+
+$(document).ready(function(){
+    $('.content').readmore({
+        speed: 175,
+        lessLink: '<a href="#">Read less</a>',
+        moreLink: '<a href="#">Read more</a>',
+        collapsedHeight: 75,
+    });
+
 });
